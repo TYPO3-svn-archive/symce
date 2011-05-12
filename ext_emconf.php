a482c1ae2e6d58ba4eac808e33b048291734182e
metti <metti@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-12 21:07:17 +0000

    * Initial import: ** Created ext with kickstarter ** Added 'records' to subtypes_addlist
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/symce/trunk@47662 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "symce".
+#
+# Auto generated 12-05-2011 22:46
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Symlink Content Elements',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Christian Opitz',
+	'author_email' => 'co@netzelf.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"c684";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"8893";s:14:"ext_tables.php";s:4:"20a8";s:13:"locallang.xml";s:4:"fd6d";s:16:"locallang_db.xml";s:4:"8e02";s:19:"doc/wizard_form.dat";s:4:"2670";s:20:"doc/wizard_form.html";s:4:"5c70";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:26:"pi1/class.tx_symce_pi1.php";s:4:"ce7b";s:34:"pi1/class.tx_symce_pi1_wizicon.php";s:4:"da5f";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"d45f";}',
+);
+
+?>
