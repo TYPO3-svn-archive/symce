0a8fbbfdbc6f58645a57aa9b84632f16e4db41a4
metti <metti@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-13 01:48:49 +0000

    * Edited ext_emconf.php, releaser to TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/symce/trunk@47674 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "symce".
 #
-# Auto generated 12-05-2011 22:46
+# Auto generated 13-05-2011 03:47
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -28,8 +28,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author_company' => '',
-	'version' => '0.0.0',
+	'author_company' => 'Netzelf - Agentur für guten Eindruck',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"c684";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"8893";s:14:"ext_tables.php";s:4:"20a8";s:13:"locallang.xml";s:4:"fd6d";s:16:"locallang_db.xml";s:4:"8e02";s:19:"doc/wizard_form.dat";s:4:"2670";s:20:"doc/wizard_form.html";s:4:"5c70";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:26:"pi1/class.tx_symce_pi1.php";s:4:"ce7b";s:34:"pi1/class.tx_symce_pi1_wizicon.php";s:4:"da5f";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"d45f";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"c684";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1495";s:17:"ext_localconf.php";s:4:"352c";s:14:"ext_tables.php";s:4:"d594";s:13:"locallang.xml";s:4:"700a";s:16:"locallang_db.xml";s:4:"0683";s:14:"doc/manual.sxw";s:4:"db9e";s:19:"doc/wizard_form.dat";s:4:"2670";s:20:"doc/wizard_form.html";s:4:"b7e3";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:26:"pi1/class.tx_symce_pi1.php";s:4:"0648";s:34:"pi1/class.tx_symce_pi1_wizicon.php";s:4:"7159";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"5bd9";}',
+	'suggests' => array(
+	),
 );
 
 ?>
